a61b9a941d9cbf52b301dd689710e8d90decbee0
JSONJuggler <25470092+JSONJuggler@users.noreply.github.com>
2021-01-16 16:45:26 -0600

    socal icons removed from footer

--- composedComponents/Footer/Footer.tsx
+++ composedComponents/Footer/Footer.tsx
@@ -1,29 +1,20 @@
-import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Copyright from './Copyright';
-import SocialsBanner from './SocialsBanner';
 
 const Footer: React.FC = () => {
   const classes = useStyles();
 
   return (
-    <Grid container component="footer" className={classes.root} justify="center" alignItems="center" direction="column">
-      <Grid container justify="center" alignItems="center">
-        <SocialsBanner />
-      </Grid>
-      <Grid item>
-        <Copyright />
-      </Grid>
-    </Grid>
+    <footer className={classes.footer}>
+      <Copyright />
+    </footer>
   );
 };
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    paddingTop: theme.spacing(2),
-    paddingBottom: theme.spacing(2),
-    marginTop: 'auto',
+  footer: {
+    padding: theme.spacing(2),
   },
 }));
 
