3c8d00a11267abc9fa42ff3dca6f1cafa046a302
JSONJuggler <25470092+JSONJuggler@users.noreply.github.com>
2021-01-24 14:19:37 -0600

    fixing footer layout, missing margins/container

--- composedComponents/Footer/Footer.tsx
+++ composedComponents/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Link, Typography } from '@material-ui/core';
+import { Container, Grid, Link, Typography } from '@material-ui/core';
 
 import Copyright from './Copyright';
 import Socials from './Socials';
@@ -10,35 +10,39 @@ const Footer: React.FC = () => {
   return (
     <>
       <Grid container className={classes.footer} alignContent="center" component="footer">
-        <Grid className={classes.footerItem} item xs={12} sm={4}>
-          <Typography variant="h6">{"Beau's Blog"}</Typography>
-        </Grid>
-        <Grid className={classes.footerItem} container item alignContent="flex-start" xs={12} sm={4}>
-          <Grid item xs={12}>
-            <Typography variant="subtitle1">{'Personal Projects'}</Typography>
+        <Container maxWidth="lg" disableGutters>
+          <Grid container item className={classes.footerItemParent} component="footer">
+            <Grid className={classes.footerItem} item xs={12} sm={4}>
+              <Typography variant="h6">{"Beau's Blog"}</Typography>
+            </Grid>
+            <Grid className={classes.footerItem} container item alignContent="flex-start" xs={12} sm={4}>
+              <Grid item xs={12}>
+                <Typography variant="subtitle1">{'Personal Projects'}</Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="subtitle2">
+                  <Link color="textPrimary" href="https://manga-back.webdeveloperbeau.com/">
+                    {'Manga API'}
+                  </Link>
+                </Typography>
+                <Typography variant="subtitle2">
+                  <Link color="textPrimary" href="https://webdeveloperbeau.com/jobbie">
+                    {'Jobbie'}
+                  </Link>
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid className={classes.footerItem} container item xs={12} sm={4}>
+              <Grid item xs={12}>
+                <Typography variant="subtitle1">{'Find me on'}</Typography>
+              </Grid>
+              <Socials />
+            </Grid>
+            <Grid item xs={12}>
+              <Copyright />
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <Typography variant="subtitle2">
-              <Link color="textPrimary" href="https://manga-back.webdeveloperbeau.com/">
-                {'Manga API'}
-              </Link>
-            </Typography>
-            <Typography variant="subtitle2">
-              <Link color="textPrimary" href="https://webdeveloperbeau.com/jobbie">
-                {'Jobbie'}
-              </Link>
-            </Typography>
-          </Grid>
-        </Grid>
-        <Grid className={classes.footerItem} container item xs={12} sm={4}>
-          <Grid item xs={12}>
-            <Typography variant="subtitle1">{'Find me on'}</Typography>
-          </Grid>
-          <Socials />
-        </Grid>
-        <Grid item xs={12}>
-          <Copyright />
-        </Grid>
+        </Container>
       </Grid>
     </>
   );
@@ -47,10 +51,12 @@ const Footer: React.FC = () => {
 const useStyles = makeStyles((theme) => ({
   footer: {
     paddingTop: theme.spacing(10),
-    padding: theme.spacing(2),
     background: theme.palette.type === 'dark' ? theme.palette.secondary.dark : theme.palette.secondary.light,
   },
   footerItem: {
+    padding: theme.spacing(2),
+  },
+  footerItemParent: {
     padding: theme.spacing(2),
   },
 }));
