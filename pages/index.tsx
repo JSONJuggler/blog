a30e7266e5e130e1b6ef2e5c8a11d82228626a6d
JSONJuggler <25470092+JSONJuggler@users.noreply.github.com>
2021-01-24 10:18:56 -0600

    making title lowercase and removing leftover google verification meta tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,13 +13,12 @@ const Home: React.FC<HomeProps> = ({ allPosts }) => {
   return (
     <>
       <Head>
-        <title>Beau Reescano | Blog</title>
+        <title>blog | beau reescano </title>
         <meta
           name="description"
           content="I'm a Full Stack web developer using the latest front-end and back-end
           technologies with a focus on Typescript, React JS, Next JS, and Node JS."
         />
-        <meta name="google-site-verification" content="x0Muns5pK71I3AK6b3XmS8zvBwcMpuSodHaI4MJqr-A" />
       </Head>
       <Landing posts={allPosts} />
     </>
