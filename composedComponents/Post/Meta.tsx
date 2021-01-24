aea1df332c6c151303912ee7f8aad7657f507834
JSONJuggler <25470092+JSONJuggler@users.noreply.github.com>
2021-01-24 10:03:10 -0600

    meta component

---
+++ composedComponents/Post/Meta.tsx
@@ -0,0 +1,42 @@
+import Head from 'next/head';
+
+import Post from '../../types/post';
+
+type MetaProps = {
+  post: Post;
+};
+
+const Meta: React.FC<MetaProps> = ({ post }) => {
+  return (
+    <Head>
+      <title>{post.title.toLocaleLowerCase()} | blog | beau reescano</title>
+      <meta prefix="og: http://ogp.me/ns#" property="og:image" content={post.ogImage.url} />
+      <meta
+        name="description"
+        content="I'm a Full Stack web developer using the latest front-end and back-end
+          technologies with a focus on Typescript, React JS, Next JS, and Node JS."
+      />
+      <meta name="author" content={post.author.name} />
+      <meta prefix="og: http://ogp.me/ns#" property="og:title" content={post.title} />
+      <meta prefix="og: http://ogp.me/ns#" property="og:description" content={post.excerpt} />
+      <meta
+        prefix="og: http://ogp.me/ns#"
+        property="og:url"
+        content={'https://webdeveloperbeau.com/blog/' + post.slug}
+      />
+
+      <meta
+        prefix="og: http://ogp.me/ns#"
+        property="og:site_name"
+        content={'https://webdeveloperbeau.com/blog/' + post.slug}
+      />
+      <meta prefix="og: http://ogp.me/ns#" property="og:image:width" content="1920" />
+      <meta prefix="og: http://ogp.me/ns#" property="og:image:height" content="960" />
+      <meta prefix="og: http://ogp.me/ns#" property="og:type" content="website" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:creator" content="JSONJuggler" />
+    </Head>
+  );
+};
+
+export default Meta;
