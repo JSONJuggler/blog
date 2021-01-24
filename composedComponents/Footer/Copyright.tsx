6c6aa3f238a318f00202d7916ad197532d67824c
JSONJuggler <25470092+JSONJuggler@users.noreply.github.com>
2021-01-24 13:13:02 -0600

    styling copyright correctly

--- composedComponents/Footer/Copyright.tsx
+++ composedComponents/Footer/Copyright.tsx
@@ -1,16 +1,36 @@
+import { makeStyles } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 
 const Copyright: React.FC = () => {
+  const classes = useStyles();
+  const isCentered = useMediaQuery('(min-width: 600px)');
+
   return (
-    <>
-      <Typography variant="caption">
-        {'Copyright © '}
-        <Link href="https://github.com/jsonjuggler">Beau Reescano</Link> {new Date().getFullYear()}
-        {'.'}
-      </Typography>
-    </>
+    <Typography
+      className={classes.root}
+      color="textSecondary"
+      variant="subtitle2"
+      align={isCentered ? 'center' : 'left'}
+    >
+      {'Copyright © '}
+      <Link color="textPrimary" href="https://github.com/jsonjuggler">
+        Beau Reescano
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 };
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    [theme.breakpoints.down('xs')]: {
+      paddingLeft: theme.spacing(2),
+      paddingRight: theme.spacing(2),
+    },
+  },
+}));
 
 export default Copyright;
