fd9dd19cb1ec5bdaf327cc6e38917594d8a02d28
JSONJuggler <25470092+JSONJuggler@users.noreply.github.com>
2021-01-24 18:18:16 -0600

    using sun and moon icon instead of switch

--- composedComponents/Navbar/ThemeToggle.tsx
+++ composedComponents/Navbar/ThemeToggle.tsx
@@ -1,57 +1,33 @@
-import { withStyles } from '@material-ui/core/styles';
 import FormGroup from '@material-ui/core/FormGroup';
-import Grid from '@material-ui/core/Grid';
-import Switch from '@material-ui/core/Switch';
+import { IconButton, Tooltip } from '@material-ui/core';
+
+import { SvgSun, SvgMoon } from '../../icons';
 
 type ThemeToggleProps = {
   toggleDarkMode: () => void;
   isDarkMode: boolean;
+  isLandingPage: boolean;
 };
 
-const ThemeToggle: React.FC<ThemeToggleProps> = ({ toggleDarkMode, isDarkMode }) => {
+const ThemeToggle: React.FC<ThemeToggleProps> = ({ toggleDarkMode, isDarkMode, isLandingPage }) => {
   return (
     <FormGroup>
-      <Grid component="label" container alignItems="center" spacing={1}>
-        <Grid item>
-          <StyledSwitch checked={!isDarkMode} onChange={toggleDarkMode} name="checked" />
-        </Grid>
-      </Grid>
+      {!isDarkMode && (
+        <Tooltip title="Toggle Dark mode">
+          <IconButton size={isLandingPage ? 'medium' : 'small'} aria-label="toggle dark mode" onClick={toggleDarkMode}>
+            <SvgSun />
+          </IconButton>
+        </Tooltip>
+      )}
+      {isDarkMode && (
+        <Tooltip title="Toggle Dark mode">
+          <IconButton size={isLandingPage ? 'medium' : 'small'} aria-label="toggle dark mode" onClick={toggleDarkMode}>
+            <SvgMoon />
+          </IconButton>
+        </Tooltip>
+      )}
     </FormGroup>
   );
 };
-
-const StyledSwitch = withStyles((theme) => ({
-  root: {
-    width: 28,
-    height: 16,
-    padding: 0,
-    display: 'flex',
-  },
-  switchBase: {
-    padding: 2,
-    color: theme.palette.grey[500],
-    '&$checked': {
-      transform: 'translateX(12px)',
-      color: theme.palette.common.white,
-      '& + $track': {
-        opacity: 1,
-        backgroundColor: theme.palette.primary.main,
-        borderColor: theme.palette.primary.main,
-      },
-    },
-  },
-  thumb: {
-    width: 12,
-    height: 12,
-    boxShadow: 'none',
-  },
-  track: {
-    border: `1px solid ${theme.palette.grey[500]}`,
-    borderRadius: 16 / 2,
-    opacity: 1,
-    backgroundColor: theme.palette.common.white,
-  },
-  checked: {},
-}))(Switch);
 
 export default ThemeToggle;
