ccc641fd11d935db2fd661f0327bbeacbf77579d
JSONJuggler <25470092+JSONJuggler@users.noreply.github.com>
2021-01-24 18:36:35 -0600

    meta tags for prototype blog

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,15 +10,26 @@ type HomeProps = {
 };
 
 const Home: React.FC<HomeProps> = ({ allPosts }) => {
+  const description = "Hi, I'm a Full Stack React and ReactNative developer and welcome to my blog!";
+
   return (
     <>
       <Head>
         <title>blog | beau reescano </title>
-        <meta
-          name="description"
-          content="I'm a Full Stack web developer using the latest front-end and back-end
-          technologies with a focus on Typescript, React JS, Next JS, and Node JS."
-        />
+        <meta name="description" content={description} />
+        <meta prefix="og: http://ogp.me/ns#" property="og:image" content="/assets/blog/blog.png" />
+
+        <meta name="author" content="Beau Reescano" />
+        <meta prefix="og: http://ogp.me/ns#" property="og:title" content="beau's blog" />
+        <meta prefix="og: http://ogp.me/ns#" property="og:description" content={description} />
+        <meta prefix="og: http://ogp.me/ns#" property="og:url" content="https://blog.webdeveloperbeau.com/" />
+
+        <meta prefix="og: http://ogp.me/ns#" property="og:site_name" content="https://blog.webdeveloperbeau.com/" />
+        <meta prefix="og: http://ogp.me/ns#" property="og:image:width" content="1920" />
+        <meta prefix="og: http://ogp.me/ns#" property="og:image:height" content="960" />
+        <meta prefix="og: http://ogp.me/ns#" property="og:type" content="website" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="JSONJuggler" />
       </Head>
       <Landing posts={allPosts} />
     </>
