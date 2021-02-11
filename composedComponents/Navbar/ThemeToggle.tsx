c7ff4b39f4e228f252c4b5261778b0a413deb982
JSONJuggler <25470092+JSONJuggler@users.noreply.github.com>
2021-02-11 09:28:30 -0600

    fixing conditional rendering ssr vs client

--- composedComponents/Navbar/ThemeToggle.tsx
+++ composedComponents/Navbar/ThemeToggle.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import FormGroup from '@material-ui/core/FormGroup';
 import { IconButton, Tooltip } from '@material-ui/core';
 
@@ -10,22 +11,16 @@ type ThemeToggleProps = {
 };
 
 const ThemeToggle: React.FC<ThemeToggleProps> = ({ toggleDarkMode, isDarkMode, isLandingPage }) => {
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => setLoaded(true), []);
+
   return (
     <FormGroup>
-      {!isDarkMode && (
-        <Tooltip title="Toggle Dark mode">
-          <IconButton size={isLandingPage ? 'medium' : 'small'} aria-label="toggle dark mode" onClick={toggleDarkMode}>
-            <SvgSun />
-          </IconButton>
-        </Tooltip>
-      )}
-      {isDarkMode && (
-        <Tooltip title="Toggle Dark mode">
-          <IconButton size={isLandingPage ? 'medium' : 'small'} aria-label="toggle dark mode" onClick={toggleDarkMode}>
-            <SvgMoon />
-          </IconButton>
-        </Tooltip>
-      )}
+      <Tooltip title="Toggle Dark mode">
+        <IconButton size={isLandingPage ? 'medium' : 'small'} aria-label="toggle dark mode" onClick={toggleDarkMode}>
+          {loaded && isDarkMode ? <SvgMoon /> : <SvgSun />}
+        </IconButton>
+      </Tooltip>
     </FormGroup>
   );
 };
